05948844580899a876dc7e30699113e983fcb1b3
jyoh <jyoh1248@gmail.com>
2026-07-23 11:38:47 +0000

    add task monitoring

--- formation_mlops_2/monitoring.py
+++ formation_mlops_2/monitoring.py
@@ -1,11 +1,12 @@
 import pandas as pd
+from datetime import datetime
 
 
 def monitor(latest_predictions: pd.DataFrame) -> pd.DataFrame:
     # Start filling function
     monitoring_df = pd.DataFrame({
-        "predictions_time": [None],
-        "predictions": [None]
+        "predictions_time": [latest_predictions["predictions_time"].max()],
+        "predictions": [latest_predictions["predictions"].mean()]
     })
     # End filling function
     return monitoring_df
